221692084690384238414dd02cf116f661751910
kvaluation <kenji.suzuki@kval.jp>
2025-12-03 02:15:39 +0900

    GitHubアイコンのリンク先を修正

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const config: DocsThemeConfig = {
   logo: <Image src="/intprofy_logo.png" alt="株式会社知的利益" width={52} height={24}/>,
   project: {
-    link: 'https://www.intprofy.co.jp',
+    link: 'https://github.com/kvaluation/intprofy-website',
   },
   docsRepositoryBase: 'https://github.com/kvaluation/intprofy-website',
   footer: {
